2c0a2de2dd5765d3f49db474c5954d632bc322eb
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2025-06-14 01:06:29 +0900

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RecoilRoot } from 'recoil';
